f83fb7ef56bdc20a32d487a86069b70a1c04bf19
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2018-06-15 22:35:10 +0530

    Added transparent StatusBar, DarkUIOverlays

--- meccpma/lib/main.dart
+++ meccpma/lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'mec_page.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+    statusBarIconBrightness: Brightness.dark
+  ));
   runApp(new MyApp());
+  Color color = Colors.transparent;
+  changeStatusColor(color);
+}
+
+changeStatusColor(Color color) async {
+  try {
+    await FlutterStatusbarcolor.setStatusBarColor(color);
+  } on PlatformException catch (e) {
+    print(e);
+  }
 }
 
 class MyApp extends StatelessWidget {
