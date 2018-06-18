141e3cfd2a10dc954e49e35405580e9205770ea6
Manikumar1998 <manikumarperla18@gmail.com>
2018-06-18 11:50:56 +0530

    Update gallery

--- meccpma/lib/gallery.dart
+++ meccpma/lib/gallery.dart
@@ -13,16 +13,27 @@ class _GalleryState extends State<Gallery> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        body: new Container(
-            alignment: Alignment.center,
-            padding: new EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 18.0),
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                options(),
-              ],
-            )));
+        body: new Column(
+
+          children: <Widget>[
+            new Expanded(
+                flex: 1,
+                child: new Container(
+                  color: Color(0xffffffff),
+                  child: options(),
+                )
+            ),
+            new Expanded(
+                flex: 9,
+                child: new Container(
+                  color: Color(0xffffffff),
+                  child: cardsGrid(),
+                )
+            )
+          ],
+
+        ),
+    );
   }
 
   Widget options() {
@@ -68,5 +79,36 @@ class _GalleryState extends State<Gallery> with SingleTickerProviderStateMixin {
               children: <Widget>[back, header, share],
             )));
     return column;
+  }
+
+  GridView cardsGrid() {
+    var grid = GridView.count(
+
+        shrinkWrap: false,
+        padding: EdgeInsets.symmetric(vertical: 1.0),
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 1.0,
+        crossAxisSpacing: 1.0,
+        children: <Widget>[
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+          Image(image: AssetImage('assets/pupp.jpg')),
+
+          ]);
+
+    return grid;
   }
 }
